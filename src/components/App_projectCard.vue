2558686ebadcd81a5d8cb3c5713685804c833549
FabrizioFavia <personal.favia@gmail.com>
2023-07-17 20:14:14 +0200

    added buttons with functions

--- src/components/App_projectCard.vue
+++ src/components/App_projectCard.vue
@@ -7,23 +7,65 @@ export default {
         return {
             api_path: 'http://127.0.0.1:8000/api/projects',
             loading: false,
-            projects: []
+            projects: [],
+            result_Page: 0,
+            total_pages: 3,
         }
     },
     mounted() {
         this.getProjects();
     }, methods: {
-        getProjects() {
+
+        getProjects(pageNumber) {
+            let config = {
+                params: {
+                    page: pageNumber,
+                }
+            };
             this.loading = true;
-            axios.get('http://127.0.0.1:8000/api/projects').then(response => {
-                console.log(response.data.results);
+            axios.get('http://127.0.0.1:8000/api/projects', config).then(response => {
                 this.projects = response.data.results;
                 console.log(this.projects);
                 this.loading = false;
+                this.result_Page = response.data.results.current_page;
+                console.log(this.result_Page);
             }).catch(err => {
                 this.loading = false;
             })
-        }
+        },
+        nextPage() {
+
+            let config = { params: { page: this.result_Page + 1 } };
+
+            if (this.result_Page < this.total_pages) {
+                this.loading = true;
+                axios.get('http://127.0.0.1:8000/api/projects', config).then(response => {
+                    this.projects = response.data.results;
+                    console.log(this.projects);
+                    this.loading = false;
+                    this.result_Page = response.data.results.current_page;
+                    console.log(this.result_Page);
+                }).catch(err => {
+                    this.loading = false;
+                })
+            }
+        },
+        prevPage() {
+
+            let config = { params: { page: this.result_Page - 1 } };
+            if (this.result_Page > 0) {
+                this.loading = true;
+                axios.get('http://127.0.0.1:8000/api/projects', config).then(response => {
+                    this.projects = response.data.results;
+                    console.log(this.projects);
+                    this.loading = false;
+                    this.result_Page = response.data.results.current_page;
+                    console.log(this.result_Page);
+                }).catch(err => {
+                    this.loading = false;
+                })
+            }
+        },
     }
 
 }
@@ -41,13 +83,16 @@ export default {
             <p>{{ project.description }}</p>
             <p>Categoria: {{ project.type.type }}</p>
             <template v-if="project.technologies.length > 0">
-                <p>Tecnologie: <span class="badge bg-primary me-2" v-for="technology in project.technologies">{{
+                <p>Tecnologie: <br><span class="badge bg-primary me-2" v-for="technology in project.technologies">{{
                     technology.name }}</span></p>
             </template>
         </div>
     </div>
     <div class="contentContainer">
-        <div class="btn btn-primary mt-4 me-2" v-for="page in projects.last_page ">{{ page }}</div>
+        <div class="btn btn-primary" @click="this.prevPage()">Precedente</div>
+        <div class="btn btn-primary mt-4 me-2" @click="this.getProjects(pageNumber)"
+            v-for="pageNumber in projects.last_page ">{{ pageNumber }}</div>
+        <div class="btn btn-primary" @click="this.nextPage()">Successiva</div>
     </div>
 </template>
 
@@ -58,7 +103,7 @@ export default {
 }
 
 .card {
-    width: 330px;
-    height: 300px;
+    width: 250px;
+    min-height: 250px;
 }
 </style>
